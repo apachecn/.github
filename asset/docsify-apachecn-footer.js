a28f9d77dd2748a6b7d88a9ddaf9a2553535d321
wizardforcel <562826179@qq.com>
2021-10-09 17:05:18 +0800

    2021-10-09 17:05:18

--- asset/docsify-apachecn-footer.js
+++ asset/docsify-apachecn-footer.js
@@ -1,7 +1,14 @@
 (function(){
-	var cnzzId = window.$docsify.cnzzId
-	var unRepo = window.$docsify.repo || ''
-	var [un, repo] = unRepo.split('/')
+    var cnzzId = window.$docsify.cnzzId
+    var unRepo = window.$docsify.repo || ''
+    var [un, repo] = unRepo.split('/')
+    var icp = ''
+        if (location.host.includes('apachecn.org')) {
+            icp = '京ICP备19016010号-1'
+        } else if (location.host.includes('ibooker.org.cn')) {
+            icp = '京ICP备2020035452号-1'
+    }
+    var icpText = icp? `<p>ICP许可证号: ${icp}</p>`: ''
     var footer = `
         <hr/>
         <div align="center">
@@ -12,6 +19,7 @@
           <iframe align="middle" src="https://ghbtns.com/github-btn.html?user=${un}&repo=${repo}&type=fork&count=true" frameborder="0" scrolling="0" width="100px" height="25px"></iframe>
           <a target="_blank" href="https://jq.qq.com/?_wv=1027&k=fgYM7eMw"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="iBooker 面试求职" title="iBooker 面试求职"></a></p>
           <p><span id="cnzz_stat_icon_${cnzzId}"></span></p>
+          ${icpText}
           <div style="text-align:center;margin:0 0 10.5px;">
             <ins class="adsbygoogle"
                  style="display:inline-block;width:728px;height:90px"
@@ -19,7 +27,7 @@
                  data-ad-slot="2543897000"></ins>
           </div>
         </div>
-	`
+    `
     var plugin = function(hook) {
       hook.afterEach(function(html) {
         return html + footer
